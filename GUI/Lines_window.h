430fd497ad8ccbbb8accf91b66ee95f4a55f7a7b
szcstmss <91480386+szcstmss@users.noreply.github.com>
2022-03-23 16:22:51 +0100

    Update Lines_window.h

--- GUI/Lines_window.h
+++ GUI/Lines_window.h
@@ -2,15 +2,20 @@
 
 using namespace Graph_lib;
 
-struct Lines_window : Graph_lib::Window {
-	Lines_window(Point xy, int w, int h, const string& title );
+
+//Rajzoló
+struct Lines_Window : Window {
+	Lines_Window(Point xy, int w, int h, const string& title);
 	Open_polyline lines;
+	Menu color_menu;
+	Menu style_menu;
 
 	bool wait_for_button();
-
 private:
 	Button next_button;
 	Button quit_button;
+	Button color_menu_button;
+	Button style_menu_button;
 
 	In_box next_x;
 	In_box next_y;
@@ -19,5 +24,26 @@ private:
 
 	void next();
 	void quit();
+
+	/*static void cb_red(Adress, Adress);
+	static void cb_blue(Adress, Adress);
+	static void cb_black(Adress, Adress);*/
+
+	void red_pressed();
+	void blue_pressed();
+	void black_pressed();
+
+	void solid_pressed();
+	void dash_pressed();
+	void dot_pressed();
+
+	void color_menu_pressed();
+	void style_menu_pressed();
+
+	void change_style(Line_style s);
+	void change_color(Color c);
+	void hide_menu();
+
+
 
 };
